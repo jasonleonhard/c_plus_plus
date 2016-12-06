1972a9e71442ab809603f9cbd47ab65ac259272f
jasonleonhard <devbrights@gmail.com>
2016-12-05 16:57:01 -0800

    add first_class calculating area

---
+++ first_class.cpp
@@ -0,0 +1,28 @@
+// g++ first_class.cpp -o first_class; ./first_class;
+
+#include <iostream>
+using namespace std;
+
+class Rectangle{
+  private:
+    int width, height;
+
+  public:
+    void set_height_and_width (int,int);
+
+    int area(){
+      return width * height;
+    }
+};
+
+void Rectangle::set_height_and_width(int x, int y){
+  width = x;
+  height = y;
+}
+
+int main(){
+  Rectangle rect;
+  rect.set_height_and_width(6,7);
+  cout << "area = " << rect.area() << endl;
+  return 0;
+}

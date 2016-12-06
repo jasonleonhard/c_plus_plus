8493fdbc9224d8683b316001e013ca22121aee1c
jasonleonhard <devbrights@gmail.com>
2016-12-05 16:43:01 -0800

    forth.cpp with alternative implementation

--- forth.cpp
+++ forth.cpp
@@ -5,11 +5,16 @@ using namespace std;
 
 int main(){
 
-  int a, b, c;
+  // int a, b, c;
+  // a = 1;
+  // b = 2;
+  // c = a + b;
 
-  a = 1;
-  b = 2;
-  c = a + b;
+// or
+
+  int a = 1;
+  int b = 2;
+  int c = a + b;
 
   cout << a << "\n" << b << "\n" << c << endl;
   cout << a + b + c << endl;
